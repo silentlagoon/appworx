9daa3edbe246f6a62f0b844b8d015886d26881cf
silentlagoon <silentlagoon@gmail.com>
2014-03-05 15:49:41 +0300

    Fixed section added possibility to change layout for logged users

--- app/views/index.php
+++ app/views/index.php
@@ -1,38 +1,42 @@
     <div class="infopanel center-block">
-        <div class="infopanel_avatar">
-            <img src="<?php echo asset('/pics/amb.jpg') ?>" class="img-circle" width="50px;" height="50px;"/>
-        </div>
-        <div class="infopanel_uderdata">
-            <div class="infopanel_uderdata_name">
-                Mary Bryant
+        <?php if(Session::has('token')) { ?>
+        <div class="info_panel_part">
+            <div class="infopanel_avatar">
+                <img src="<?php echo asset('/pics/amb.jpg') ?>" class="img-circle" width="50px;" height="50px;"/>
             </div>
-            <div class="infopanel_uderdata_birthdate">
-                July 5, 1965
+            <div class="infopanel_uderdata">
+                <div class="infopanel_uderdata_name">
+                    Mary Bryant
+                </div>
+                <div class="infopanel_uderdata_birthdate">
+                    July 5, 1965
+                </div>
+            </div>
+            <div class="infopanel_lastvisit">
+                <strong>Last visit:</strong>
+                <div class="infopanel_lastvisit_date">
+                    Oct. 1, 2013
+                </div>
+            </div>
+            <div class="infopanel_enrolledid">
+                <strong>Enrolled ID:</strong>
+                <div class="infopanel_enrolledid_name">
+                    Tom Smith
+                </div>
+            </div>
+            <div ng-controller="ModalDemoCtrl">
+                <div class="infopanel_edit_profile_button">
+                    <button class="btn btn-default btn-sm" ng-click="open()">Edit Profile</button>
+                </div>
+            </div>
+            <div class="infopanel_archive_button">
+                <button class="btn btn-default btn-sm">Archive</button>
+            </div>
+            <div class="infopanel_report_button">
+                <button class="btn btn-success btn-sm">Report</button>
             </div>
         </div>
-        <div class="infopanel_lastvisit">
-            <strong>Last visit:</strong>
-            <div class="infopanel_lastvisit_date">
-                Oct. 1, 2013
-            </div>
-        </div>
-        <div class="infopanel_enrolledid">
-            <strong>Enrolled ID:</strong>
-            <div class="infopanel_enrolledid_name">
-                Tom Smith
-            </div>
-        </div>
-        <div ng-controller="ModalDemoCtrl">
-            <div class="infopanel_edit_profile_button">
-                <button class="btn btn-default btn-sm" ng-click="open()">Edit Profile</button>
-            </div>
-        </div>
-        <div class="infopanel_archive_button">
-            <button class="btn btn-default btn-sm">Archive</button>
-        </div>
-        <div class="infopanel_report_button">
-            <button class="btn btn-success btn-sm">Report</button>
-        </div>
+        <?php } ?>
         <div class="left_panels" style="float:left;">
             <div class="diagmosis">
                 <div class="diagnosis_header">Diagnosis</div>
